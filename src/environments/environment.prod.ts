509c7a9d1c8998daa5936e99abefdd4c6618ab5f
jafars <jafarshod99@gmail.com>
2023-01-19 14:22:20 +0700

    environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAtYRFS1BeXsu7wk5A13w61QYqXPJ3DzRc",
+    authDomain: "angularauth-50d43.firebaseapp.com",
+    projectId: "angularauth-50d43",
+    storageBucket: "angularauth-50d43.appspot.com",
+    messagingSenderId: "300453839775",
+    appId: "1:300453839775:web:c7cf0804614b0d8663b3aa",
+    databaseURL: "https://angularauth-50d43-default-rtdb.firebaseio.com",  
+  }
 };
